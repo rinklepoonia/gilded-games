4adecd29bd64c70d3a5b520a9e71c54d194bea21
rinklepoonia <rinklepoonia889@gmail.com>
2024-07-12 10:49:08 +0530

    chng col in utility section

--- src/components/Utility.jsx
+++ src/components/Utility.jsx
@@ -6,7 +6,7 @@ function Utility() {
     <div className="lg:bg-full bg-cover xl:pb-[194px] lg:pb-[23px] pb-[10px] mt-[-1px] bg-[url('./assets/images/webp/Utility-bg-img.webp')]">
       <div className="container xl:px-0 px-3">
         <div className="row items-center">
-          <div data-aos="fade-right" className="xl:w-[41.66%] w-full px-3">
+          <div data-aos="fade-right" className="xl:w-[50%] w-full px-3">
             <h2 className="font-Josefin font-bold text-[48px] leading-[61px] text-[#7E5FCA] xl:text-start text-center lg:mb-4 sm:mb-2 mb-0">
               NFT <span className="text-white">Utility</span>
             </h2>
@@ -22,7 +22,7 @@ function Utility() {
               magna duis imperdiet posuere iaculis.
             </p>
           </div>
-          <div data-aos="fade-left" className="xl:w-[58.33%] w-full  px-3">
+          <div data-aos="fade-left" className="xl:w-[50%] w-full  px-3">
             <div className="flex justify-center align-center">
               <img
                 className="w-full max-w-[369px] sm:h-[638px] h-[inherit] object-cover"
